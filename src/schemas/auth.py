1957dd40706333f9dabcf28b71bbcda78249a8b3
Bustiks <geldish238@gmail.com>
2026-04-03 02:47:37 +0300

    feat: create auth and user schemas

---
+++ src/schemas/auth.py
@@ -0,0 +1,33 @@
+from pydantic import BaseModel, ConfigDict, Field, EmailStr
+from src.schemas.user import UserRead
+
+class RegistrationRequest(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+    email: EmailStr
+    username: str = Field(min_length=3, max_length=20)
+    password: str = Field(min_length=4)
+
+class RegistrationResponse(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+    user: UserRead
+    access_token: str
+    refresh_token: str
+    token_type: str = "bearer"
+
+class LoginRequest(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+    username: str = Field(min_length=3, max_length=20)
+    password: str = Field(min_length=4)
+
+class LoginResponse(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+    user: UserRead
+    access_token: str
+    refresh_token: str
+    token_type: str = "bearer"
+
+class RefreshResponse(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+    access_token: str
+    refresh_token: str
+    token_type: str = "bearer"
---
+++ src/schemas/user.py
@@ -0,0 +1,29 @@
+from pydantic import BaseModel, ConfigDict, EmailStr, field_validator
+from src.utils.annotated import Locale
+
+class UserRead(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+    id: int
+    email: EmailStr
+    username: str
+    locale: Locale | None = None
+
+class UserUpdate(BaseModel):
+    email: EmailStr | None = None
+    username: str | None = None
+    password: str | None = None
+    locale: Locale | None = None
+
+    @field_validator("username")
+    @classmethod
+    def validate_username(cls, value: str | None):
+        if value is not None and not value.strip():
+            raise ValueError("Username cannot be empty")
+        return value
+    
+    @field_validator("password")
+    @classmethod
+    def validate_password(cls, value: str | None):
+        if value is not None and len(value) < 8:
+            raise ValueError("Password must be at least 8 character long")
+        return value
